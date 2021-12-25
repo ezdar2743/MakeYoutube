47cb63185c3f2127997095159c66bdf6c24341f7
ezdar@naver.com <ezdar@naver.com>
2021-12-25 16:43:20 +0900

    Show popular video only text

--- make-youtube/src/app.jsx
+++ make-youtube/src/app.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return (
-    <h1>hello</h1>
-  );
+  const [videos, setVideos] = useState([]);
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&MaxResults=25&key=AIzaSyChm1YXsoOBSx9vR3Bz64aZZ2oCXNPXIZQ", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  },[]);
+  return <VideoList videos ={videos}></VideoList>
+   
 }
 
 export default App;
