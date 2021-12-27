78111c2a075d69c71fe546b0d9011897946fa8a7
ezdar@naver.com <ezdar@naver.com>
2021-12-27 17:19:11 +0900

    Complete search bar design

--- make-youtube/src/components/search_header/search_header.jsx
+++ make-youtube/src/components/search_header/search_header.jsx
@@ -4,12 +4,13 @@ import styles from './search_header.module.css';
 const Search_header = (props) => {
     return (
         <header className={styles.header}>
-            <img className={styles.logo} src="./images/logo.png" alt="logo" />
+            <a href="./"><img className={styles.logo} src="./images/logo.png" alt="logo" /></a>
             <h1 className={styles.title}>Youtube</h1>
             <input className={styles.input} type="text" placeholder='Search' />
             <button className={styles.btn} type="submit">
                 <img src="./images/search.png" alt="search" />
             </button>
+            
         </header>
     )
 }
