3af13379eaf8c1d7dcfd9ee60e1201d7c548eb67
ezdar@naver.com <ezdar@naver.com>
2021-12-29 23:53:03 +0900

    Add youtube video src

--- make-youtube/src/components/video_select/video_select.jsx
+++ make-youtube/src/components/video_select/video_select.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
-import Styles from './video_select.module.css'
+import styles from './video_select.module.css'
 const VideoSelect = ({video}) => (
-            <h1>{video.snippet.title}</h1>
+            <section className={styles.detail}>
+                <iframe
+                className={styles.video}
+                type="text/html" 
+                width="100%" 
+                height="500px"
+                src={`https://www.youtube.com/embed/${video.id}`}
+                frameborder="0" allowfullscreen>
+
+                </iframe>
+            <h2>{video.snippet.title}</h2>
+            <h3>{video.snippet.channelTitle}</h3>
+            <pre>{video.snippet.description}</pre>
+            
+            </section>
+            
     );
 
 export default VideoSelect;
