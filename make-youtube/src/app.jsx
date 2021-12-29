42999a754689b386b55337d4b046a9d7ee923113
ezdar@naver.com <ezdar@naver.com>
2021-12-29 12:48:01 +0900

    Make search's class for Refactoring and Hide Api's key

--- make-youtube/src/app.jsx
+++ make-youtube/src/app.jsx
@@ -3,30 +3,15 @@ import Search_header from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 import styles from './app.module.css';
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
   const search = query =>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&&key=AIzaSyChm1YXsoOBSx9vR3Bz64aZZ2oCXNPXIZQ`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item, id : item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube.search(query)
+    .then(videos => setVideos(videos))
   };
   useEffect(()=>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyChm1YXsoOBSx9vR3Bz64aZZ2oCXNPXIZQ", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube.mostPopular()
+    .then(videos => setVideos(videos));
   },[]);
   return <div className={styles.app}>
   <Search_header onSearch={search}></Search_header> 
